f7b407c03eaab5b6d74c5b1b4cecdfce45f9c443
Cheater121 <106826455+Cheater121@users.noreply.github.com>
2023-04-18 10:51:21 +0300

    Added logging

--- video_saver_tg_bot_pytube.py
+++ video_saver_tg_bot_pytube.py
@@ -1,5 +1,6 @@
 import telebot
 import os
+import logging
 from pytube import YouTube
 
 bot = telebot.TeleBot(os.environ['TELEGRAM_BOT_TOKEN'])
@@ -11,21 +12,34 @@ def start_handler(message):
 
 @bot.message_handler(func=lambda message: True)
 def download_video(message):
+        #adding logger
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.ERROR)
+        handler = logging.FileHandler('error.log')
+        handler.setLevel(logging.ERROR)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        
         if not message.text.startswith("http"):
         	bot.send_message(message.chat.id, "Please send a valid link.")
         	return
+        
         try:
         	yt = YouTube(message.text)
         	stream = yt.streams.filter(progressive=True, file_extension='mp4').first()
         	bot.send_message(message.chat.id, "Starting download video.")
         	stream.download(filename='video.mp4')
-        except TypeError: #need to find all exeptions
+        except Exception as e:
+        	logger.exception(f"{e}\n")
         	bot.send_message(message.chat.id, "Could not download the video. Please try again with a different link.")
         	return
+        
         try:
         	with open("video.mp4", "rb") as f:
         		bot.send_video(message.chat.id, f)
-        except FileNotFoundError:
+        except Exception as e: #FileNotFoundError:
+        	logger.exception(f"{e}\n")
         	bot.send_message(message.chat.id, "Could not find the downloaded video.")
         finally:
         	os.remove("video.mp4")
